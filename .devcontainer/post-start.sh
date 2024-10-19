bfe0fa8c46c063fa350ef27555b84300ad96b490
Isaac Adams <isaacdadams@pm.me>
2024-10-19 18:15:58 +0000

    safely loading gpg configuration from host to ensure gpg signs commits correctly

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -ex
 
-WORKSPACE_DIR=$1
+_WORKSPACE_DIR=$1
+_GNUPGHOME=$2
 
-git config --global --add safe.directory $WORKSPACE_DIR
+git config --global --add safe.directory $_WORKSPACE_DIR
+
+rsync -av \
+    --exclude '*.lock' \
+    --exclude '*.lock.*' \
+    --exclude '.#lk0x*' \
+    /tmp/.gnupg /root
+
+chmod 700 /root/.gnupg
